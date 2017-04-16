33bd7a56353b3210f2f15f997837c3f6a2ff82f2
Mark Sun <marksun1988@gmail.com>
2017-04-16 11:11:10 -0700

    change entrypoint

--- app/entrypoint.js
+++ app/entrypoint.js
@@ -12,12 +12,7 @@ import { Route, Router, browserHistory } from 'react-router';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <Route path="/story" component={StoryReader} />
-        <Route path="/not_found" render={() => <h2>NOT FOUND</h2>} />
-      </Route>
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('content')
 );
